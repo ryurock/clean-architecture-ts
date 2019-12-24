fd007775c4fee05ce37f0069a9ee89f37196db3a
ryurock <ryusukekimura3@gmail.com>
2019-12-25 01:28:18 +0900

    event subscriptionの設定途中まで

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ TypeScript slackbot clean-architecture
 ## SlackBotに関する情報
 
 https://www.dkrk-blog.net/slack/slack_api01
+
+## Event Subscription
+
+### challenge
+
+Slack APPの疎通確認みたいなもの
+
+localで実行する場合は
+[ngrok](https://ngrok.com/)を使用して一旦疎通確認を取る
+
+```
+npm run ngrok:start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-// import { WebClient, WebAPICallResult } from '@slack/web-api'
-// const yaml = require('js-yaml')
-// const fs   = require('fs')
+import { Request, Response } from "express";
+
 import { SecretDriver } from './drivers/secret-driver'
 import { ChatMessageRepository } from './repository/chat-mesasge-respository'
 
-
-export const helloWorld = (req: any, res: any) => {
-  const secretDriver = new SecretDriver('./secret.yaml')
-  const repository = new ChatMessageRepository(secretDriver)
-  repository.postMessage({
-    text: 'test',
-    channel: '#slack-ts-bot'
-  })
-  res.send('Hello, World');
+export const slackbot = async (req: Request, res: Response) => {
+  console.log(req.params)
+  console.log(req.body)
+  console.log(req.path)
+  if (req.path == '/event/chalenge') {
+    return res.send(req.body.challenge);
+  }
+  // const secretDriver = new SecretDriver('./secret.yaml')
+  // const repository = new ChatMessageRepository(secretDriver)
+  // repository.postMessage({
+  //   text: 'test',
+  //   channel: '#slack-ts-bot'
+  // })
+  // res.send('Hello, World');
 };
